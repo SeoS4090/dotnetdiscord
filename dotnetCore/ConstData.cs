35f63748bcf4605c65d2f0b9c9d404f314e97c27
seoseunghyun <ab4090@angames.co.kr>
2022-01-19 16:58:06 +0900

    초기화
    import discord.net

---
+++ dotnetCore/ConstData.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class ConstData
+{
+    public const string BOT_TOKEN = "OTMzMjUwMDgyMDI1OTI2NzE2.Yeey3g.VG1BJ8jUm2DuYvU1VPdaiEFxZl8";
+}
